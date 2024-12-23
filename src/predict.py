ff66358159f3ca39c962768640755dc105b6f8a8
1cbyc <isaacnsisong@gmail.com>
2024-12-23 20:56:27 +0100

    i just want everyone to confirm their illness

--- src/predict.py
+++ src/predict.py
@@ -3,12 +3,12 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-# Load the trained model
+# to load the trained model
 model = load_model('final_model.keras')
 
-# Define the function for prediction
+# to define the function for prediction
 def predict_image(image_path):
-    img = load_img(image_path, target_size=(224, 224))  # Resize the image to match the input size
+    img = load_img(image_path, target_size=(224, 224))  # will resize the image to match the input size
     img_array = img_to_array(img) / 255.0  # Normalize pixel values
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
 
