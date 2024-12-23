0a6cd17156771a027e030cdfced93518a7096962
1cbyc <isaacnsisong@gmail.com>
2024-12-23 21:03:29 +0100

    i just want everyone to confirm their illness

--- src/predict.py
+++ src/predict.py
@@ -18,7 +18,7 @@ def predict_image(image_path):
     return class_label, confidence
 
 # how i will use it
-image_path = 'tests/image.jpg tests/scans/IM-0029-0001.jpeg'  # for now, will manually replace with the path to your test image
+image_path = 'tests/scans/IM-0029-0001.jpeg'  # for now, will manually replace with the path to test image
 if os.path.exists(image_path):
     label, confidence = predict_image(image_path)
     print(f"Prediction: {label} (Confidence: {confidence:.2f})")
