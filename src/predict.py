def3b78bd57671f182154671a05df9f47fe1f443
1cbyc <isaacnsisong@gmail.com>
2024-12-24 19:13:15 +0100

    i just want everyone to confirm their illness

--- src/predict.py
+++ src/predict.py
@@ -4,7 +4,9 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 # to load the trained model
-model = load_model('final_model.keras')
+model = load_model('best_model.keras')
+
+
 
 # to define the function for prediction
 def predict_image(image_path):
